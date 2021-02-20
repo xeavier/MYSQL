1b06129d8a105464faff06a2c6c36a93e1a4479b
xeavier1 <chandarmtsb007@gmail.com>
2021-02-20 21:20:06 +0530

    all.php update

--- all.php
+++ all.php
@@ -7,13 +7,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT firstName, lastName FROM users";
+$sql = "SELECT firstName, lastName gender FROM users";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo  " - Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>";
+    echo  " - Name: " . $row["firstName"]. " " . $row["lastName"]. " " . $row["gender"]."<br>";
   }
 } else {
   echo "0 results";
